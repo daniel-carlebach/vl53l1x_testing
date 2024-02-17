a0d92f550cab7cfe935ef2b2ae933763b3270b38
Daniel Carlebach <danieldcarlebach@gmail.com>
2024-02-17 16:30:20 -0500

    Automatically plot all files

--- plot_measurement.py
+++ plot_measurement.py
@@ -1,8 +1,11 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("data/walking_through_door_slowly.csv")
-
-sns.lineplot(data=df, x="Time (s)", y="Distance (mm)")
-plt.savefig("walking_through_door_slowly.png", dpi=300)
+for file in os.listdir("data"):
+    if file.endswith(".csv"):
+        df = pd.read_csv("data/" + file)
+        sns.lineplot(data=df, x="Time (s)", y="Distance (mm)")
+        plt.savefig("figures/" + file.removesuffix(".csv") + ".png", dpi=300)
+        plt.clf()
